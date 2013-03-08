e6b0d394406ec91172b01e1d05407fc3043e32f1
System Administrator <root@Taras-iPad.(none)>
2013-03-08 16:35:39 -0600

    Wrote a small app that pulls a git repository when server is hit by a post request.

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , p = require('child_process');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+TARAS_BLOG_DIR = '~/TarasBlog';
+
+app.get('/hook', function(req, res){
+p.exec('cd ' + TARAS_BLOG_DIR + " && git pull');
+res.send('hello world');
+});
